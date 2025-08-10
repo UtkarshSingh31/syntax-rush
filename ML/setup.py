0ddb7f9cf0eeec28f9c36d957b96cb80d6a6002a
Utkarsh Singh <utkarshsingh.0902@gmail.com>
2025-08-10 10:37:08 +0530

    Add initial project structure with requirements and setup files.(ml)

---
+++ ML/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN='-e .'
+def get_packages(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements.
+    '''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if HYPEN in requirements:
+            requirements.remove(HYPEN)
+
+    return requirements
+
+setup(
+    name='ML',
+    version='0.0.1',
+    author='Utkarsh',
+    author_email='utkarshsingh.0902@gmail.com',
+    packages=find_packages(),
+    install_requires=get_packages('requirements.txt')
+)
